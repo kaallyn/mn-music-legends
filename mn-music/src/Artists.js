abb4ad32021044c4af7e5405e37b783cdd37f224
Katie Allyn <kaallyn@gmail.com>
2017-08-01 00:20:44 -0500

    updates

--- mn-music/src/Artists.js
+++ mn-music/src/Artists.js
@@ -6,17 +6,16 @@ class Artists extends Component {
     return (
       <section className="artists">
         <nav>
-          <ul>
-            <li>Prince</li>
-            <li>The Replacements</li>
-            <li>Soul Asylum</li>
+          <ul className="cl-effect-1">
+            <li><a>Prince</a></li>
+            <li><a>The Replacements</a></li>
+            <li><a>Soul Asylum</a></li>
           </ul>
+          <div className='divider'></div>
         </nav>
-        <div className="hero-content">
-          <hgroup>
-            <p>lorem</p>
-
-          </hgroup>
+        <div className="artist-content">
+          <h3>Artist Name</h3>
+          <p>lorem</p>
 
         </div>
       </section>
